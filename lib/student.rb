1e4211b1779342d178ca714ff3cac79b49c99073
Jumi Yoon <jums.code@gmail.com>
2023-03-08 13:33:42 -0800

    commit tree 607d2019b6cc925517f2c9a6f6396232f02208ae

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "./user"
+
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(learn)
+        @knowledge.push(learn)
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
